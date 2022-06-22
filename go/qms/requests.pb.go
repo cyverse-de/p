a92e1904a3f9539d10ce0608e18795f1eb22ebf4
John Wregglesworth <wregglej@cyverse.org>
2022-06-22 11:34:29 -0700

    Add AddUsage and GetUsages messages for QMS

--- go/qms/requests.pb.go
+++ go/qms/requests.pb.go
@@ -202,6 +202,141 @@ func (x *IsOverageRequest) GetResourceName() string {
 	return ""
 }
 
+type AddUsage struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Header       *header.Header `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
+	Username     string         `protobuf:"bytes,2,opt,name=username,proto3" json:"username,omitempty"`
+	ResourceName string         `protobuf:"bytes,3,opt,name=resource_name,proto3" json:"resource_name,omitempty"`
+	// Possible values are defined in the database, so we can't use an enum here
+	UpdateType string  `protobuf:"bytes,4,opt,name=update_type,proto3" json:"update_type,omitempty"`
+	UsageValue float32 `protobuf:"fixed32,5,opt,name=usage_value,proto3" json:"usage_value,omitempty"`
+}
+
+func (x *AddUsage) Reset() {
+	*x = AddUsage{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_qms_requests_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *AddUsage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*AddUsage) ProtoMessage() {}
+
+func (x *AddUsage) ProtoReflect() protoreflect.Message {
+	mi := &file_qms_requests_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use AddUsage.ProtoReflect.Descriptor instead.
+func (*AddUsage) Descriptor() ([]byte, []int) {
+	return file_qms_requests_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *AddUsage) GetHeader() *header.Header {
+	if x != nil {
+		return x.Header
+	}
+	return nil
+}
+
+func (x *AddUsage) GetUsername() string {
+	if x != nil {
+		return x.Username
+	}
+	return ""
+}
+
+func (x *AddUsage) GetResourceName() string {
+	if x != nil {
+		return x.ResourceName
+	}
+	return ""
+}
+
+func (x *AddUsage) GetUpdateType() string {
+	if x != nil {
+		return x.UpdateType
+	}
+	return ""
+}
+
+func (x *AddUsage) GetUsageValue() float32 {
+	if x != nil {
+		return x.UsageValue
+	}
+	return 0
+}
+
+type GetUsages struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Header   *header.Header `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
+	Username string         `protobuf:"bytes,2,opt,name=username,proto3" json:"username,omitempty"`
+}
+
+func (x *GetUsages) Reset() {
+	*x = GetUsages{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_qms_requests_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetUsages) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetUsages) ProtoMessage() {}
+
+func (x *GetUsages) ProtoReflect() protoreflect.Message {
+	mi := &file_qms_requests_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetUsages.ProtoReflect.Descriptor instead.
+func (*GetUsages) Descriptor() ([]byte, []int) {
+	return file_qms_requests_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *GetUsages) GetHeader() *header.Header {
+	if x != nil {
+		return x.Header
+	}
+	return nil
+}
+
+func (x *GetUsages) GetUsername() string {
+	if x != nil {
+		return x.Username
+	}
+	return ""
+}
+
 var File_qms_requests_proto protoreflect.FileDescriptor
 
 var file_qms_requests_proto_rawDesc = []byte{
@@ -228,10 +363,26 @@ var file_qms_requests_proto_rawDesc = []byte{
 	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x75, 0x73, 0x65, 0x72,
 	0x6e, 0x61, 0x6d, 0x65, 0x12, 0x24, 0x0a, 0x0d, 0x72, 0x65, 0x73, 0x6f, 0x75, 0x72, 0x63, 0x65,
 	0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0d, 0x72, 0x65, 0x73,
-	0x6f, 0x75, 0x72, 0x63, 0x65, 0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x20, 0x5a, 0x1e, 0x67, 0x69,
-	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x79, 0x76, 0x65, 0x72, 0x73, 0x65,
-	0x2d, 0x64, 0x65, 0x2f, 0x70, 0x2f, 0x67, 0x6f, 0x2f, 0x71, 0x6d, 0x73, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x6f, 0x75, 0x72, 0x63, 0x65, 0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x22, 0xb1, 0x01, 0x0a, 0x08, 0x41,
+	0x64, 0x64, 0x55, 0x73, 0x61, 0x67, 0x65, 0x12, 0x1f, 0x0a, 0x06, 0x68, 0x65, 0x61, 0x64, 0x65,
+	0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x07, 0x2e, 0x48, 0x65, 0x61, 0x64, 0x65, 0x72,
+	0x52, 0x06, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x12, 0x1a, 0x0a, 0x08, 0x75, 0x73, 0x65, 0x72,
+	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x75, 0x73, 0x65, 0x72,
+	0x6e, 0x61, 0x6d, 0x65, 0x12, 0x24, 0x0a, 0x0d, 0x72, 0x65, 0x73, 0x6f, 0x75, 0x72, 0x63, 0x65,
+	0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0d, 0x72, 0x65, 0x73,
+	0x6f, 0x75, 0x72, 0x63, 0x65, 0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x20, 0x0a, 0x0b, 0x75, 0x70,
+	0x64, 0x61, 0x74, 0x65, 0x5f, 0x74, 0x79, 0x70, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x0b, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x5f, 0x74, 0x79, 0x70, 0x65, 0x12, 0x20, 0x0a, 0x0b,
+	0x75, 0x73, 0x61, 0x67, 0x65, 0x5f, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x05, 0x20, 0x01, 0x28,
+	0x02, 0x52, 0x0b, 0x75, 0x73, 0x61, 0x67, 0x65, 0x5f, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x48,
+	0x0a, 0x09, 0x47, 0x65, 0x74, 0x55, 0x73, 0x61, 0x67, 0x65, 0x73, 0x12, 0x1f, 0x0a, 0x06, 0x68,
+	0x65, 0x61, 0x64, 0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x07, 0x2e, 0x48, 0x65,
+	0x61, 0x64, 0x65, 0x72, 0x52, 0x06, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x12, 0x1a, 0x0a, 0x08,
+	0x75, 0x73, 0x65, 0x72, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08,
+	0x75, 0x73, 0x65, 0x72, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x20, 0x5a, 0x1e, 0x67, 0x69, 0x74, 0x68,
+	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x79, 0x76, 0x65, 0x72, 0x73, 0x65, 0x2d, 0x64,
+	0x65, 0x2f, 0x70, 0x2f, 0x67, 0x6f, 0x2f, 0x71, 0x6d, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -246,22 +397,26 @@ func file_qms_requests_proto_rawDescGZIP() []byte {
 	return file_qms_requests_proto_rawDescData
 }
 
-var file_qms_requests_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_qms_requests_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_qms_requests_proto_goTypes = []interface{}{
 	(*AllUserOveragesRequest)(nil),      // 0: AllUserOveragesRequest
 	(*UserResourceOveragesRequest)(nil), // 1: UserResourceOveragesRequest
 	(*IsOverageRequest)(nil),            // 2: IsOverageRequest
-	(*header.Header)(nil),               // 3: Header
+	(*AddUsage)(nil),                    // 3: AddUsage
+	(*GetUsages)(nil),                   // 4: GetUsages
+	(*header.Header)(nil),               // 5: Header
 }
 var file_qms_requests_proto_depIdxs = []int32{
-	3, // 0: AllUserOveragesRequest.header:type_name -> Header
-	3, // 1: UserResourceOveragesRequest.header:type_name -> Header
-	3, // 2: IsOverageRequest.header:type_name -> Header
-	3, // [3:3] is the sub-list for method output_type
-	3, // [3:3] is the sub-list for method input_type
-	3, // [3:3] is the sub-list for extension type_name
-	3, // [3:3] is the sub-list for extension extendee
-	0, // [0:3] is the sub-list for field type_name
+	5, // 0: AllUserOveragesRequest.header:type_name -> Header
+	5, // 1: UserResourceOveragesRequest.header:type_name -> Header
+	5, // 2: IsOverageRequest.header:type_name -> Header
+	5, // 3: AddUsage.header:type_name -> Header
+	5, // 4: GetUsages.header:type_name -> Header
+	5, // [5:5] is the sub-list for method output_type
+	5, // [5:5] is the sub-list for method input_type
+	5, // [5:5] is the sub-list for extension type_name
+	5, // [5:5] is the sub-list for extension extendee
+	0, // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_qms_requests_proto_init() }
@@ -306,6 +461,30 @@ func file_qms_requests_proto_init() {
 				return nil
 			}
 		}
+		file_qms_requests_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*AddUsage); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_qms_requests_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GetUsages); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -313,7 +492,7 @@ func file_qms_requests_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_qms_requests_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
