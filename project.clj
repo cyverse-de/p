5364d3fa38f2e8f892c20d3437f4fe82677cbbec
John Wregglesworth <wregglej@cyverse.org>
2022-07-22 11:01:58 -0700

    Add project.clj file to build and package the generated java code

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.cyverse.de/cyverse-de-protobufs "0.0.1-SNAPSHOT"
+  :description "Code generated from the DE protocol buffer definitions"
+  :url "https://github.com/cyverse-de/p"
+  :license {:name "BSD"
+            :url "https://cyverse.org/license"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clojusc/protobuf "3.5.1-v1.1"]
+                 [com.google.protobuf/protobuf-java "3.19.4"]]
+  :java-source-paths ["java"])
