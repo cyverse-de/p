3c76ad09e6b502bee7b45fcdaa67968d1eb8cced
John Wregglesworth <wregglej@cyverse.org>
2023-03-04 14:26:32 -0700

    Add comments to the unit testing

--- debuff/src/lib.rs
+++ debuff/src/lib.rs
@@ -7,22 +7,30 @@ mod test {
 
     #[test]
     fn test_header() {
+        // Initialize a header.
         let mut h = Header::default();
 
-        let v = header::Value {
-            value: vec![String::from("value")],
-        };
+        // Initialize a value and set it.
+        let mut v = header::Value::default();
+        v.value.push(String::from("value"));
 
+        // Insert the value into the header map.
         h.map.insert(String::from("foo"), v);
 
+        // clone the header for comparisons later.
         let test_value = h.clone();
 
+        // serialize and deserialize the header to/from JSON
         let s = serde_json::to_string(&h).unwrap();
         let u: Header = serde_json::from_str(&s).unwrap();
+
+        // get the value from the deserialized header
         let u_val = u.map.get("foo").unwrap();
 
+        // Pull the value from the cloned header
         let v_val = test_value.map.get("foo").unwrap();
 
+        // make sure the cloned value matches the deserialized value
         assert_eq!(u_val.value[0], v_val.value[0]);
     }
 }
