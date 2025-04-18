fb586696f4482b34a5aa3c293c4a243ea1f27607
Paul Sarando <psarando@cyverse.org>
2025-04-18 16:09:30 -0700

    Add :deploy-repositories and :release-tasks to project.clj
    Added :release-tasks mainly to add a `java/v` prefix to release tags,
    and also for custom versioning commit messages.

--- project.clj
+++ project.clj
@@ -8,4 +8,14 @@
                  [com.google.protobuf/protobuf-java "4.29.0"]
                  [com.google.protobuf/protobuf-java-util "4.29.0"]]
   :plugins [[lein-ancient "0.7.0"]]
-  :java-source-paths ["java"])
+  :java-source-paths ["java"]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit" "java/v%s release"]
+                  ["vcs" "tag" "java/v"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit" "Bump java version to %s"]
+                  ["vcs" "push"]])
